7323b3b48914a95ff67979d3c67131e3e90aa9a4
amadea.son <amadea.son@edu-poly.com>
2022-11-14 17:39:10 +0900

    [손현지]MySQL yaml 세팅[Backend]

--- backend/backend/src/main/java/com/example/backend/controller/BasicBoardController.java
+++ backend/backend/src/main/java/com/example/backend/controller/BasicBoardController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequestMapping("/api") //경로 분리해둬도 돌아가는구나
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+//CrossOrigin은 있으나 없으나 결과가 같네?
 public class BasicBoardController {
 
-    @PostMapping("/api/users")
+    @PostMapping("/users")
     public User user() {
         System.out.println("UserApiController start");
         User user = new User(1, "손현지","kps!1234","amadea.son@edu-poly.com");
