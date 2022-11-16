959d5d2d961decc0ec90a6bc6ca081414a187fad
amadea.son <amadea.son@edu-poly.com>
2022-11-16 10:18:24 +0900

    [손현지] jpa로 table생성 확인 springSecurity 제외 환경설정 완료 [back]

--- backend/backend/src/main/java/com/example/backend/entity/BasicBoard.java
+++ backend/backend/src/main/java/com/example/backend/entity/BasicBoard.java
@@ -1,24 +1,24 @@
-//package com.example.backend.entity;
-//
-//import lombok.Data;
-//import javax.persistence.*;
-//
-//@Data
-//@Entity
-//public class BasicBoard {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long boardNo;
-//
-//    @Column
-//    private String title;
-//
-//    @Column
-//    private String writer;
-//
-//    @Column
-//    private String password;
-//
-//    @Column String Content;
-//}
+package com.example.backend.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+public class BasicBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardNo;
+
+    @Column
+    private String title;
+
+    @Column
+    private String writer;
+
+    @Column
+    private String password;
+
+    @Column String Content;
+}
