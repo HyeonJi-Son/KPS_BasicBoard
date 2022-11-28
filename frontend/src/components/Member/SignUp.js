49790e66b25997b7e2c7409a8c8c316321a85789
amadea.son <amadea.son@edu-poly.com>
2022-11-28 17:54:34 +0900

    [손현지] 회원가입 Reducer 및 UI 작성(data잘 들어가는지 확인 필요) [MSU]

--- frontend/src/components/Member/SignUp.js
+++ frontend/src/components/Member/SignUp.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Button, Form, Input, Select } from 'antd'
+import { useDispatch } from 'react-redux';
+import { signUp } from '../../reducer/memberReducer';
 //import styles from './Layout.module.css';
 
 export function SignUp() {
+    const [data, setData] = useState({});
+    //비어있는 처음 값 + 변화가 감지된 값 모두 useState에 넣어준다.
+
+    const dispatch = useDispatch;
+
+    const changeInput = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }   //기존 data에서 이벤트(input내용 변화) 감지된 name항목의 value를 추가하여 setData라고 해준다.
 
     const { Option } = Select;
 
     const onFinish = (values) => {
         console.log('Success:', values);
       };
-      const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-      };
+    
+    const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+    };
+
+
+    const submitForm = (e) => { //form이 새로고침 될 때
+        e.preventDefault();
+        dispatch(signUp(data)); //signup
+    }
+
+
 
     return (
-        <div align="center">
+        <form align="center" onSubmit={submitForm}>
             <Link to="/">
                 <Button type="default" htmlType="submit">
                     로그인
@@ -30,8 +49,7 @@ export function SignUp() {
                 initialValues={{ remember: true, }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
+                autoComplete="off">
 
                 <Form.Item
                     label="Nickname"
@@ -41,9 +59,8 @@ export function SignUp() {
                         required: true,
                         message: 'Please input your Nickname!',
                     },
-                    ]}
-                >
-                    <Input placeholder="  닉네임을 입력하세요."/>
+                ]}>
+                    <Input placeholder="  닉네임을 입력하세요." onChange={changeInput}/>
                 </Form.Item>
 
                 <Form.Item
@@ -54,9 +71,8 @@ export function SignUp() {
                         required: true,
                         message: 'Please input your Email Address!',
                     },
-                    ]}
-                >
-                    <Input.Group compact>
+                ]}>
+                    <Input.Group compact onChange={changeInput}>
                         <Input
                             style={{
                             width: '60%',
@@ -79,9 +95,8 @@ export function SignUp() {
                         required: true,
                         message: 'Please input your password!',
                     },
-                    ]}
-                >
-                    <Input.Password placeholder="  비밀번호를 입력하세요."/>
+                ]}>
+                    <Input.Password placeholder="  비밀번호를 입력하세요." onChange={changeInput}/>
                 </Form.Item>
 
                 <Form.Item
@@ -92,14 +107,12 @@ export function SignUp() {
                         required: true,
                         message: 'Please input your password!',
                     },
-                    ]}
-                >
+                ]}>
                     <Input.Password placeholder="  비밀번호를 확인합니다."/>
                 </Form.Item>
 
                 <Form.Item
-                    wrapperCol={{ offset: 8, span: 16, }}
-                    >
+                    wrapperCol={{ offset: 8, span: 16, }}>
 
                     <Link to="">
                         <Button type="primary" htmlType="submit">
@@ -111,7 +124,7 @@ export function SignUp() {
 
             </Form>
 
-        </div>
+        </form>
     )
 }
 
--- frontend/src/reducer/memberReducer.js
+++ frontend/src/reducer/memberReducer.js
@@ -1,14 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 //import axios from 'axios';
 
 const memberSlice = createSlice({
     name: 'member',
     initialState: {
-
+        members: [], //배열로 들어옴
+        member: null, //객체로 들어옴
     },
     reducers: {
-
+        setMembers: (state, action) => {
+            state.members = [...action.payload] //배열로 들어오는 형태   
+        },
+        setMember: (state, action) => {
+            state.member = {...action.payload} //객체로 들어오는 형태
+        }
     },
 });
 
+//memberSlice.reducer를 다른 곳에서도 사용할 수 있도록 export 달아주었음.
 export default memberSlice.reducer;
+
+//회원가입 reducer의 기본적인 형식은 게시글 등록과 같다.
+export const signUp = (data) => () => {
+    axios
+        .post('member', data) //postMapping 타고 가게 될 것임.
+        .then(() => {
+            alert("회원 가입 성공!")
+            window.location = '/'; //로그인 페이지로 이동
+        }, (error) => {
+            console.error(error);
+        })
+}
